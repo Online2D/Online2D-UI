8f8a154fb1f213c12ebf57c852b5c50a31a5ab54
Sergio Garibaldi <sdaniel.garibaldi@gmail.com>
2024-05-29 18:55:57 -0300

    Update
    -Added settings button into connect window
    -Added username into account creation window
    -Changed "email" for "username" into login box

--- js/connect.js
+++ js/connect.js
@@ -9,6 +9,7 @@ document.on('ready',function() {
 
     // Close create account modal
     document.$('.modal .create-account .wrapper .header .mini-button').on('click', function() {
+        document.$('.modal .create-account input[name="username"]').value = '';
         document.$('.modal .create-account input[name="email"]').value = '';
         document.$('.modal .create-account input[name="password"]').value = '';
         document.$('.modal .create-account input[name="confirm_password"]').value = '';
@@ -33,12 +34,13 @@ document.on('ready',function() {
 
     // Create account button action
     document.$('#account_create').on('click', function() {
+        const username = document.$('.modal .create-account .field input[name="username"]').value;
         const email = document.$('.modal .create-account .field input[name="email"]').value;
         const password = document.$('.modal .create-account .field input[name="password"]').value;
         const confirmPassword = document.$('.modal .create-account .field input[name="confirm_password"]').value;
         const tos = document.$('.modal .create-account .special-field input[name="tos"]').checked;
     
-        if (email.length === 0 || password.length === 0 || confirmPassword.length === 0) {
+        if (username.length === 0 || email.length === 0 || password.length === 0 || confirmPassword.length === 0) {
             globalShowError(document, '.modal .create-account .wrapper .system', 'master', 'create_account', 'error_empty_field');
             return;
         }
